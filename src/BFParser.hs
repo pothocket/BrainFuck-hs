09788f92b99da2be8304973adc658cd0c461eb34
pothocket <pothocket1@gmail.com>
2017-04-08 02:00:44 +0100

    Complete parser

--- src/BFParser.hs
+++ src/BFParser.hs
@@ -3,23 +3,47 @@ module BFParser where
 import Data.Maybe (mapMaybe)
 import Control.Monad (sequence)
 import Data.Char
+import Control.Applicative (many, (<|>))
+import Control.Monad.Trans.Maybe
 
 import BFTypes
 import Parser
 
-getToken :: Char -> Maybe Token
-getToken c = case c of
-    '+' -> Just TInc
-    '-' -> Just TDec
-    '<' -> Just TLeft
-    '>' -> Just TRight
-    '.' -> Just TPrint
-    ',' -> Just TLeft
-    '[' -> Just TLoopL
-    ']' -> Just TLoopR
-    _   -> Nothing
+tokenMap :: [(Char, Token)]
+tokenMap = [ ('+', TInc)
+           , ('-', TDec)
+           , ('<', TLeft)
+           , ('>', TRight)
+           , ('.', TPrint)
+           , (',', TGet)
+           , ('[', TLoopL)
+           , (']', TLoopR)
+           ]
 
-operator :: Parser ParseError Token Token
-operator = anyOf operatorTokens
+exprMap :: [(Token, Expr)]
+exprMap = [ (TInc, OpInc)
+          , (TDec, OpDec)
+          , (TLeft, OpLeft)
+          , (TRight, OpRight)
+          , (TPrint, IOPrint)
+          , (TGet, IOGet)
+          ]
+
+getTokens :: String -> [Token]
+getTokens = mapMaybe . flip lookup $ tokenMap
+
+getExpr :: Token -> Maybe Expr 
+getExpr = flip lookup exprMap
+
+operator :: Parser ParseError Token Expr 
+operator = parseMaybe $ getExpr <$> anyOf operatorTokens
 
 bfParser :: Parser ParseError Token [Expr]
+bfParser = many $ loop bfParser <|> operator
+
+loop :: Parser ParseError Token [Expr] -> Parser ParseError Token Expr
+loop p = do
+    oneOf TLoopL
+    ts <- p
+    oneOf TLoopR
+    return $ Loop ts
