52e3ff9395c0912ea6225c90f3e4f325143644d3
pothocket <pothocket1@gmail.com>
2017-04-12 20:55:40 +0100

    Begin evaluation of BF programs

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,5 +1,29 @@
-module Eval where 
+module Eval (evalBF, initArray) where 
 
-import BFTypes
+import Control.Applicative
+import Control.Monad ((>=>))
+
 import ListZipper
+import BFTypes
+import BFParser
 
+initArray :: BFArray
+initArray = mkZipper 0
+
+dispatch :: ([Expr] -> BFProgram) -> Expr -> BFProgram
+dispatch e expr = case expr of
+    OpInc   -> return <$> modify succ
+    OpDec   -> return <$> modify (subtract 1)
+    OpLeft  -> return <$> zLeft
+    OpRight -> return <$> zRight
+    IOGet   -> \xs -> do
+        n <- getLine
+        return $ set (read n) xs
+    IOPrint -> liftA2 (>>) (putStrLn . show . getCursor) return
+    Loop xs -> e xs
+
+evalBF :: [Expr] -> BFArray -> IO BFArray
+evalBF = foldr1 (>=>) . fmap (dispatch evalLoop)
+
+evalLoop :: [Expr] -> BFProgram
+evalLoop = undefined
